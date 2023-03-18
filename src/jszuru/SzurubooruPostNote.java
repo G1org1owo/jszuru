0614f270002b07953adfac052802a0ea246921ac
G1org1owo <giorgio.torchio26@gmail.com>
2023-03-18 23:36:49 +0100

    Added class SzurubooruPostNote

---
+++ src/jszuru/SzurubooruPostNote.java
@@ -0,0 +1,52 @@
+package jszuru;
+
+import java.util.List;
+import java.util.Map;
+
+public class SzurubooruPostNote {
+    protected List<Point> points;
+    protected String text;
+
+    protected class Point{
+        protected float x;
+        protected float y;
+
+        protected Point(float x, float y){
+            this.x = x;
+            this.y = y;
+        }
+
+        public float getX() {
+            return x;
+        }
+        public Point setX(float x) {
+            this.x = x;
+            return this;
+        }
+
+        public float getY() {
+            return y;
+        }
+        public Point setY(float y) {
+            this.y = y;
+            return this;
+        }
+    }
+
+    public SzurubooruPostNote(List<List<Float>> polygon, String text){
+        this.points = polygon.stream().map(x -> new Point(x.get(0), x.get(1))).toList();
+        this.text = text;
+    }
+
+    public List<Point> getPoints() {
+        return points;
+    }
+    public String getText() {
+        return text;
+    }
+
+    public Map<String, Object> json(){
+        return Map.of("polygon", points.stream().map(point -> List.of(point.x, point.y)).toList(),
+                "text", text);
+    }
+}
