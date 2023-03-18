9aa66fb1a49b673489c02b21e5d1b13bce667d68
G1org1owo <giorgio.torchio26@gmail.com>
2023-03-18 23:37:05 +0100

    Added class SzurubooruSearchResult

---
+++ src/jszuru/SzurubooruSearchResult.java
@@ -0,0 +1,23 @@
+package jszuru;
+
+public class SzurubooruSearchResult {
+    protected SzurubooruPost post;
+    protected float distance;
+    protected boolean exact;
+
+    public SzurubooruSearchResult(SzurubooruPost post, float distance, boolean exact){
+        this.post = post;
+        this.distance = distance;
+        this.exact = exact;
+    }
+
+    public SzurubooruPost getPost() {
+        return post;
+    }
+    public float getDistance() {
+        return distance;
+    }
+    public boolean isExact() {
+        return exact;
+    }
+}
